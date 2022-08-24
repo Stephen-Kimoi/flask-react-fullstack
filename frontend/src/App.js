f416feadc2c3003d0f2a22dd5806bcb6563d5b04
Steve Kimoi <moenisteve@gmail.com>
2022-08-24 09:46:50 +0300

    displaying the data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,9 @@ const baseUrl = 'http://localhost:5000'
 
 function App() {
   const [description, setDescription] = useState(""); 
+  const [editDescription, setEditDescription] = useState(""); 
   const [eventsList, setEventsList] = useState([]); 
+  const [eventId, setEventId] = useState(null); 
 
   const fetchEvents = async () => {
     const data = await axios.get(`${baseUrl}/events`); 
@@ -16,8 +18,12 @@ function App() {
     setEventsList(events); 
   }
 
-  const handleChange = e => {
-    setDescription(e.target.value); 
+  const handleChange = (e, field)=> {
+    if (field === 'edit') {
+      setEditDescription(e.target.value); 
+    } else {
+      setDescription(e.target.value)
+    }
   }
 
   const handleDelete = async (id) => {
@@ -29,12 +35,32 @@ function App() {
     }
   }
 
+  const handleEdit = (event) => {
+    setEditDescription(event.description); 
+    setEventId(event.id); 
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault(); 
     try {
-      const data = await axios.post(`${baseUrl}/events`, {description}); 
-      setEventsList(...eventsList, data.data); 
-      setDescription(''); 
+      if (editDescription) {
+        const data = await axios.put(`${baseUrl}/events/${eventId}`, {description: editDescription})
+        const updatedEvent = data.data.event; 
+        const updatedList = eventsList.map( event => {
+          if (event.id === eventId) {
+            return event = updatedEvent
+          } else {
+            return event
+          }
+        })
+        setEventsList(updatedList);  
+      } else {
+        const data = await axios.post(`${baseUrl}/events`, {description}); 
+        setEventsList(...eventsList, data.data); 
+      }
+      setDescription('');
+      setEditDescription(''); 
+      setEventId(null); 
     } catch (err) {
       console.error(err.message)
     }
@@ -50,23 +76,44 @@ function App() {
           <form onSubmit={handleSubmit}>
              <label htmlFor='description'>Description</label>
              <input 
-               onChange={handleChange} 
+               onChange={handleChange}
                type="text" 
                name="description" 
                id="description"
+               placeholder='Describe the event'
                value={description}
              />
              <button type='submit'>Submit</button>
           </form>
           <div className='eventsSection'>
              <ul>
-               { eventsList.map( events => {
+               { eventsList.map( (events) => {
+                 if (eventId === events.id) {
+                    return (
+                      <li>
+                        <form onSubmit={handleSubmit} key={events.id}>
+                            <input 
+                              onChange={ (e) => handleChange(e,'edit')} 
+                              type="text" 
+                              name="editDescription" 
+                              id="editDescription"
+                              placeholder={events.description}
+                              value={editDescription}
+                            /> 
+                        <button type='submit'>Submit</button>    
+                        </form>
+                    </li>
+                    )
+                 } else {
                   return (
                     <li key={events.id} style={{ display: 'flex'}}>
+                      { format(new Date(events.created_at), "MM/dd - p")} : {" "}
                       {events.description}
+                      <button onClick={ () => handleEdit(events)}>Edit</button>
                       <button onClick={ () => handleDelete(events.id)}>x</button>
                     </li>
                   )
+                 }
                })}
              </ul>
           </div>
